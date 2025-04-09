d6d5e29402c31ef3eedc66272b59a8c7797d9c71
MdAsraful56 <work.mdasraful56@gmail.com>
2025-04-09 08:40:55 +0600

    count data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import './App.css'
-import Header from './components/Header/Header'
+import Main from './layouts/Main/Main'
+import Login from './components/Login/Login'
+import Checkout from './components/Checkout/Checkout'
+import Inventory from './components/Inventory/Inventory'
+import Orders from './components/Orders/Orders'
+import cartProductsLoader from './loaders/cartProductsLoader'
 import Shop from './components/Shop/Shop'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={ <Main/> }>
+        <Route index element={ <Shop/> } loader={ () => fetch(`http://localhost:5000/productsCount`) } />
+        <Route path='orders' element={ <Orders/> } loader={ cartProductsLoader } />
+        <Route path='inventory' element={ <Inventory/> } />
+        <Route path='checkout' element={ <Checkout/> } />
+        <Route path='login' element={ <Login/> } />
+      </Route>
+    )
+  )
+
 
   return (
-    <div className="App">
-      <Header></Header>
-      <Shop></Shop>
-    </div>
+    <>
+      <RouterProvider router={ router } />
+    </>
   )
 }
 
