3d220447504218cd41689817ebee05c15619dd23
MdAsraful56 <work.mdasraful56@gmail.com>
2025-04-12 18:14:58 +0600

    load some data show

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -131,10 +131,10 @@ const Shop = () => {
                     </div>
                     <select className='Options-2' value={itemsPerPage} onChange={ handleItemsPerPage }>
                         <option value="10">10</option>
+                        <option value="15">15</option>
                         <option value="20">20</option>
+                        <option value="25">25</option>
                         <option value="30">30</option>
-                        <option value="40">40</option>
-                        <option value="50">50</option>
                     </select>
                 </div>
             </div>
